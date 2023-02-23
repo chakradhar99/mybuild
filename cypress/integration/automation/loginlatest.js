08a68d73d095718b480ee6d0867dfd4bd9b9a96b
Chakradhar papanaboina <60740582+chakradhar99@users.noreply.github.com>
2023-02-23 16:48:22 +0530

    Update loginlatest.js

--- cypress/integration/automation/loginlatest.js
+++ cypress/integration/automation/loginlatest.js
@@ -6,13 +6,13 @@ describe('Login', () => {
     it('displays the login form', () => {
         cy.get('[data-cy="input-email-login-page"]').should('be.visible');
         cy.get('[data-cy="input-password-login-page"]').should('be.visible');
-        cy.get('[data-cy="btn-submit-login-page"]').should('be.visible');
+        cy.get('.eEQhhG').should('be.visible');
     });
   
     it('fails to login with invalid credentials', () => {
         cy.get('[data-cy="input-email-login-page"]').type('invalid@example.com');
         cy.get('[data-cy="input-password-login-page"]').type('invalidpassword');
-        cy.get('[data-cy="btn-submit-login-page"]').click();
+        cy.get('.eEQhhG').click();
 
       cy.get('.toast-font').should('be.visible');
     });
@@ -20,8 +20,9 @@ describe('Login', () => {
     it('logs in with valid credentials', () => {
         cy.get('[data-cy="input-email-login-page"]').type('chakradhar.papanaboina@cawstudios.com');
         cy.get('[data-cy="input-password-login-page"]').type('Kiwi@123');
-        cy.get('[data-cy="btn-submit-login-page"]').click();
+        cy.get('.eEQhhG').click();
       cy.url().should('include', '/inbox');
     });
   });
+
   
